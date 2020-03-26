7a8b3de212fdcb60441ea564feb2db2d64e7f4fc
Adeyemo Peter <peteradeyemo66@gmail.com>
2020-03-26 16:21:34 +0100

    my second commit

--- app/views/customers/dashboard.php
+++ app/views/customers/dashboard.php
@@ -95,7 +95,7 @@ $key = $data['key'];
                             
                             
                             <ul class="widget widget-menu unstyled">
-                                <li><a href="ui-button-icon.html"><i class="menu-icon icon-bold"></i> Buttons </a></li>
+                                <li><a href="ui-button-icon.html"><i class="menu-icon icon-bold"></i> Work </a></li>
                                 <li><a href="ui-typography.html"><i class="menu-icon icon-book"></i>Typography </a></li>
                                 <li><a href="form.html"><i class="menu-icon icon-paste"></i>Forms </a></li>
                                 <li><a href="table.html"><i class="menu-icon icon-table"></i>Tables </a></li>
